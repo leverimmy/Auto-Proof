7d9c193efdeaf6e6a5d2fc448cc65d9525878100
LeverImmy <506503360@qq.com>
2023-10-27 17:14:38 +0800

    feat: more general

--- main.cpp
+++ main.cpp
@@ -2,23 +2,23 @@
 
 class Term {
 public:
-    char l, r;
-    int op;
-    Term(char _l, int _op, char _r) : l(_l), r(_r), op(_op) {}
+    char op;
+    Term *l, *r;
+    Term(Term* _l, char _op, Term* _r) : l(_l), op(_op), r(_r) {}
 };
 
 class Judgement {
 public:
-    std::queue<Term> l, r;
+    std::queue<Term*> l, r;
 };
 
-bool isReduced(std::queue<Term> qTerm) {
+bool isReduced(std::queue<Term*> qTerm) {
     bool flag = true;
-    std::queue<Term> tmp = qTerm;
+    std::queue<Term*> tmp = qTerm;
     while (!tmp.empty()) {
-        Term t = tmp.front();
+        Term* t = tmp.front();
         tmp.pop();
-        if (t.op != 0) {
+        if (t->op == '!' || t->op == '&' || t->op == '|' || t->op == '>' || t->op == '=') {
             flag = 0;
             break;
         }
@@ -26,12 +26,27 @@ bool isReduced(std::queue<Term> qTerm) {
     return flag;
 }
 
-void print(std::queue<Term> qTerm) {
-    std::queue<Term> tmp = qTerm;
+void printTree(Term* u) {
+    if (u->l != nullptr) {
+        putchar('(');
+        printTree(u->l);
+        putchar(')');
+    }
+    putchar(u->op);
+    if (u->r != nullptr) {
+        putchar('(');
+        printTree(u->r);
+        putchar(')');
+    }
+}
+
+void print(std::queue<Term*> qTerm) {
+    std::queue<Term*> tmp = qTerm;
     while (!tmp.empty()) {
-        Term t = tmp.front();
+        Term* t = tmp.front();
         tmp.pop();
-        printf("%c %d %c\n", t.l, t.op, t.r);
+        printTree(t);
+        puts("");
     }
 }
 
@@ -43,122 +58,147 @@ bool check(Judgement j) {
     print(j.r);
     puts("---End---");
     if (!isReduced(j.l)) {
-        Term tmp = j.l.front();
-        if (tmp.op == 0) {
+        Term* tmp = j.l.front();
+        if (tmp->op == '!') {
+            Judgement x = j;
+            x.l.pop();
+            x.r.push(tmp->r);
+            return check(x);
+        } else if (tmp->op == '&') {
+            Judgement x = j;
+            x.l.pop();
+            x.l.push(tmp->l);
+            x.l.push(tmp->r);
+            return check(x);
+        } else if (tmp->op == '|') {
+            Judgement x = j, y = j;
+            x.l.pop(), y.l.pop();
+            x.l.push(tmp->l);
+            y.l.push(tmp->r);
+            return check(x) && check(y);
+        } else if (tmp->op == '>') {
+            Judgement x = j, y = j;
+            x.l.pop(), y.l.pop();
+            x.r.push(tmp->l);
+            y.l.push(tmp->r);
+            return check(x) && check(y);
+        } else if (tmp->op == '=') {
+            Judgement x = j, y = j;
+            x.l.pop(), y.l.pop();
+            x.l.push(tmp->l);
+            x.l.push(tmp->r);
+            y.r.push(tmp->l);
+            y.r.push(tmp->r);
+            return check(x) && check(y);
+        } else {
             Judgement x = j;
             x.l.pop();
             x.l.push(tmp);
             return check(x);
-        } else if (tmp.op == 1) {
-            Judgement x = j;
-            x.l.pop();
-            x.r.push(Term(0, 0, tmp.r));
-            return check(x);
-        } else if (tmp.op == 2) {
-            Judgement x = j;
-            x.l.pop();
-            x.l.push(Term(0, 0, tmp.l));
-            x.l.push(Term(0, 0, tmp.r));
-            return check(x);
-        } else if (tmp.op == 3) {
-            Judgement x = j, y = j;
-            x.l.pop(), y.l.pop();
-            x.l.push(Term(0, 0, tmp.l));
-            y.l.push(Term(0, 0, tmp.r));
-            return check(x) && check(y);
-        } else if (tmp.op == 4) {
-            Judgement x = j, y = j;
-            x.l.pop(), y.l.pop();
-            x.r.push(Term(0, 0, tmp.l));
-            y.l.push(Term(0, 0, tmp.r));
-            return check(x) && check(y);
-        } else if (tmp.op == 5) {
-            Judgement x = j, y = j;
-            x.l.pop(), y.l.pop();
-            x.l.push(Term(0, 0, tmp.l));
-            x.l.push(Term(0, 0, tmp.r));
-            y.r.push(Term(0, 0, tmp.l));
-            y.r.push(Term(0, 0, tmp.r));
-            return check(x) && check(y);
         }
     }
         
     if (!isReduced(j.r)) {
-        Term tmp = j.r.front();
-        if (tmp.op == 0) {
+        Term* tmp = j.r.front();
+        if (tmp->op == '!') {
+            Judgement x = j;
+            x.r.pop();
+            x.l.push(tmp->r);
+            return check(x);
+        } else if (tmp->op == '&') {
+            Judgement x = j, y = j;
+            x.r.pop(), y.r.pop();
+            x.r.push(tmp->l);
+            y.r.push(tmp->r);
+            return check(x) && check(y);
+        } else if (tmp->op == '|') {
+            Judgement x = j;
+            x.r.pop();
+            x.r.push(tmp->l);
+            x.r.push(tmp->r);
+            return check(x);
+        } else if (tmp->op == '>') {
+            Judgement x = j;
+            x.r.pop();
+            x.l.push(tmp->l);
+            x.r.push(tmp->r);            
+            return check(x);
+        } else if (tmp->op == '=') {
+            Judgement x = j, y = j;
+            x.r.pop(), y.r.pop();
+            x.l.push(tmp->l);
+            x.r.push(tmp->r);
+            y.l.push(tmp->l);
+            y.r.push(tmp->r);            
+            return check(x) && check(y);
+        } else {
             Judgement x = j;
             x.r.pop();
             x.r.push(tmp);
             return check(x);
-        } else if (tmp.op == 1) {
-            Judgement x = j;
-            x.r.pop();
-            x.l.push(Term(0, 0, tmp.r));
-            return check(x);
-        } else if (tmp.op == 2) {
-            Judgement x = j, y = j;
-            x.r.pop(), y.r.pop();
-            x.r.push(Term(0, 0, tmp.l));
-            y.r.push(Term(0, 0, tmp.r));
-            return check(x) && check(y);
-        } else if (tmp.op == 3) {
-            Judgement x = j;
-            x.r.pop();
-            x.r.push(Term(0, 0, tmp.l));
-            x.r.push(Term(0, 0, tmp.r));
-            return check(x);
-        } else if (tmp.op == 4) {
-            Judgement x = j;
-            x.r.pop();
-            x.l.push(Term(0, 0, tmp.l));
-            x.r.push(Term(0, 0, tmp.r));
-            return check(x);
-        } else if (tmp.op == 5) {
-            Judgement x = j, y = j;
-            x.r.pop(), y.r.pop();
-            x.l.push(Term(0, 0, tmp.l));
-            x.r.push(Term(0, 0, tmp.r));
-            y.l.push(Term(0, 0, tmp.l));
-            y.r.push(Term(0, 0, tmp.r));
-            return check(x) && check(y);
         }
     }
 
 
     std::set<char> s;
 
-    std::queue<Term> tmpp = j.l;
+    std::queue<Term*> tmpp = j.l;
     while (!tmpp.empty()) {
-        Term x = tmpp.front();
+        Term* x = tmpp.front();
         tmpp.pop();
-        s.insert(x.r);
+        s.insert(x->op);
     }
     tmpp = j.r;
     while (!tmpp.empty()) {
-        Term x = tmpp.front();
+        Term* x = tmpp.front();
         tmpp.pop();
-        if (s.count(x.r) != 0)
+        if (s.count(x->op) != 0)
             return true;
     }
     return false;
 }
 
 int main() {
-    /*Term a = Term(0, 1, 'Q');
-    Term b = Term('P', 4, 'Q');
-    Term c = Term(0, 1, 'P');
-    Judgement jj;
-    jj.l.push(a), jj.l.push(b);
-    jj.r.push(c);
-    puts(check(jj) ? "Yes" : "No");*/
 
-    Term a = Term('P', 3, 'Q');
-    Term b = Term('P', 4, 'R');
-    Term c = Term('Q', 4, 'S');
-    Term d = Term('S', 3, 'R');
+    // e.g 1
+    Term *a = new Term((Term*)nullptr, 'Q', (Term*)nullptr);
+    Term *b = new Term((Term*)nullptr, '!', a); // !Q
+    Term *c = new Term((Term*)nullptr, 'P', (Term*)nullptr);
+    Term *d = new Term((Term*)nullptr, 'Q', (Term*)nullptr);
+    Term *e = new Term(c, '>', d); // P > Q
+    Term *f = new Term(b, '&', e); // (!Q) & (P > Q)
+
+    Term *g = new Term((Term*)nullptr, 'P', (Term*)nullptr);
+    Term *h = new Term((Term*)nullptr, '!', g); // !P
+
     Judgement jj;
-    jj.l.push(a), jj.l.push(b), jj.l.push(c);
-    jj.r.push(d);
+    jj.l.push(f);
+    jj.r.push(h);
+
     puts(check(jj) ? "Yes" : "No");
+
+    /*// e.g 2
+    Term *a = new Term((Term*)nullptr, 'P', (Term*)nullptr);
+    Term *b = new Term((Term*)nullptr, 'Q', (Term*)nullptr);
+    Term *c = new Term(a, '|', b); // P | Q
+    Term *d = new Term((Term*)nullptr, 'P', (Term*)nullptr);
+    Term *e = new Term((Term*)nullptr, 'R', (Term*)nullptr);
+    Term *f = new Term(d, '>', e); // P > R
+    Term *g = new Term(c, '&', f); // (P | Q) & (P > R)
+    Term *h = new Term((Term*)nullptr, 'Q', (Term*)nullptr);
+    Term *i = new Term((Term*)nullptr, 'S', (Term*)nullptr);
+    Term *j = new Term(h, '>', i); // Q > S
+    Term *k = new Term(g, '&', j); // ((P | Q) & (P > R)) & (Q > S)
+
+    Term *l = new Term((Term*)nullptr, 'S', (Term*)nullptr);
+    Term *m = new Term((Term*)nullptr, 'R', (Term*)nullptr);
+    Term *n = new Term(l, '|', m); // S | R
+
+    Judgement jj;
+    jj.l.push(k);
+    jj.r.push(n);
+
+    puts(check(jj) ? "Yes" : "No");*/
+    
     return 0;
 }
